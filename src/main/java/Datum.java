946731de5e1e850855c174aa1d606e0842442b8d
teundejong1 <58029921+teundejong1@users.noreply.github.com>
2020-05-15 12:28:54 +0200

    Datum, setters, getters, constructors

--- src/main/java/Datum.java
+++ src/main/java/Datum.java
@@ -7,20 +7,57 @@ public class Datum {
 	/**
 	 * Constructor
 	 */
-	// TODO
-
-	public boolean bestaatDatum(int dag, int maand, int jaar) {
-		// TODO
-		return false;
+	public Datum(int dag, int maand, int jaar) {
+		this.dag = dag;
+		this.maand = maand;
+		this.jaar = jaar;
 	}
 
-	/**
-	 * Getter voor Sting weergave van datum
-	 *
-	 * @return Geboortedatum
-	 */
-	public String getDatumAsString() {
-		// TODO
-		return "";
+	public Datum() {
+		this.dag = 0;
+		this.maand = 0;
+		this.jaar = 0;
 	}
-}
+
+	public void setDag(int dag) {
+		this.dag = dag;
+	}
+
+	public void setMaand(int maand) {
+		this.maand = maand;
+	}
+
+	public void setJaar(int jaar) {
+		this.jaar = jaar;
+	}
+
+	public int getDag () {
+			return dag;
+		}
+
+	public int getMaand () {
+			return maand;
+		}
+
+	public int getJaar () {
+			return jaar;
+		}
+
+
+	public boolean bestaatDatum ( int dag, int maand, int jaar){
+			// TODO
+			return false;
+		}
+
+		/**
+		 * Getter voor Sting weergave van datum
+		 *
+		 * @return Geboortedatum
+		 */
+	public String getDatumAsString () {
+			// TODO
+			return "";
+		}
+
+
+	}
