dd3424b47e2308d96acf30fcad2a32cc72fc5682
teundejong1 <58029921+teundejong1@users.noreply.github.com>
2020-05-15 12:15:35 +0200

    1e bewerkingen in klasse Artikel

--- src/main/java/Artikel.java
+++ src/main/java/Artikel.java
@@ -1,4 +1,14 @@
 public class Artikel {
     private String naam;
     private float prijs;
+
+    public Artikel(String naam, float prijs) {
+        this.naam = naam;
+        this.prijs = prijs;
+}
+    public Artikel() {
+        this.naam = naam;
+        this.prijs = prijs;
+
+    }
 }
