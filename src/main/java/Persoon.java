51d46fa00eab489a9be7309b632e8c9e07566292
teundejong1 <58029921+teundejong1@users.noreply.github.com>
2020-05-15 13:19:32 +0200

    Datum, setters, getters, constructors

---
+++ src/main/java/Persoon.java
@@ -0,0 +1,78 @@
+import java.util.Objects;
+
+public class Persoon {
+    private int bsn;
+    private String voornaam;
+    private String achternaam;
+    private String geboortedatum;
+    private char geslacht;
+
+    public Persoon(int bsn, String voornaam, String achternaam, String geboortedatum, char geslacht) {
+        this.bsn = bsn;
+        this.voornaam = voornaam;
+        this.achternaam = achternaam;
+        this.geboortedatum = geboortedatum;
+        if (checkGeslacht(geslacht)) {
+            this.geslacht = geslacht;
+        } else {
+            System.out.println(geslacht + "Dit is geen geldig geslacht");
+        }
+    }
+    public String getGeslacht () {
+
+        if (checkGeslacht(geslacht)) {
+            String strGeslacht = String.valueOf(geslacht);
+            strGeslacht = strGeslacht.toUpperCase();
+            if (Objects.equals(strGeslacht, "M")) {
+                strGeslacht = "Man";
+            } else if (Objects.equals(strGeslacht, "V")) {
+                strGeslacht = "Vrouw";
+            } else {
+                strGeslacht = "Onbekend";
+            }
+            return strGeslacht;
+        }
+
+        return " ";
+    }
+
+    public void setbsn ( int bsn){
+        this.bsn = bsn;
+    }
+
+    public void setVoornaam (String voornaam){
+        this.voornaam = voornaam;
+    }
+
+    public void setAchternaam (String achternaam){
+        this.achternaam = achternaam;
+    }
+
+    public void setGeboortedatum (String geboortedatum){
+        this.geboortedatum = geboortedatum;
+    }
+
+    public void setGeslach ( char geslacht){
+        this.geslacht = geslacht;
+
+    }
+    private boolean checkGeslacht ( char geslacht){
+        return geslacht == 'M' || geslacht == 'm' || geslacht == 'V' || geslacht == 'v';
+    }
+
+    public int getBsn() {
+        return bsn;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public String getGeboortedatum() {
+        return geboortedatum;
+    }
+}
